14d388cadc1c5a47020b4d415ecabf8264699772
Ivo Saca <ivosaca@gmail.com>
2025-11-30 01:46:00 -0300

    desafio 2 finalizado

--- desafio_dois/infrastructure/df_estoque_mov.py
+++ desafio_dois/infrastructure/df_estoque_mov.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import datetime
+import json
 
 
 class movEstoque:
@@ -9,7 +10,6 @@ class movEstoque:
         self.estoque_inicial_json = os.path.join(
             self.cwd + "/desafio_dois/dados/estoque_inicial.json"
         )
-        self.horario_mov = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         self.estoque_atual_json = os.path.join(
             self.cwd + "/desafio_dois/dados/estoque_atual.json"
         )
@@ -18,16 +18,16 @@ class movEstoque:
         """Geração do dataframe de estoque a partir do arquivo JSON."""
         data = pd.read_json(self.estoque_inicial_json)
         df_estoque = pd.json_normalize(data["estoque"])
-        df_estoque["horario_mov"] = self.horario_mov
+        horario = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        df_estoque["horario_mov"] = horario
         df_estoque["codigoProduto"] = df_estoque["codigoProduto"].astype(int)
         df_estoque["estoque"] = df_estoque["estoque"].astype(int)
         return df_estoque
 
     def df_estoque_atual(self):
-        """Geração do dataframe de estoque atual a partir do arquivo JSON."""
+        """Geração do dataframe de estoque a partir do arquivo JSON."""
         data = pd.read_json(self.estoque_atual_json)
         df_estoque = pd.json_normalize(data["estoque"])
-        df_estoque["horario_mov"] = self.horario_mov
         df_estoque["codigoProduto"] = df_estoque["codigoProduto"].astype(int)
         df_estoque["estoque"] = df_estoque["estoque"].astype(int)
         return df_estoque
@@ -36,6 +36,10 @@ class movEstoque:
         """Filtragem do dataframe pelo código do produto selecionado."""
         df_estoque = self.df_estoque_atual()
         df_produto_selecionado = df_estoque.loc[df_estoque["codigoProduto"] == produto]
+        df_produto_selecionado = df_produto_selecionado.iloc[-1:]
+        df_produto_selecionado["horario_mov"] = datetime.datetime.now().strftime(
+            "%Y-%m-%d %H:%M:%S"
+        )
         if df_produto_selecionado.empty:
             continuar = input(
                 "Erro: Código do produto não encontrado no estoque. Gostaria de tentar novamente? (s/n)\n"
@@ -54,6 +58,10 @@ class movEstoque:
         print(f"\nMovimentando produto...\n")
         if tipo_mov.lower() == "entrada":
             df_mov = df_estoque.loc[df_estoque["codigoProduto"] == produto]
+            df_mov = df_mov.iloc[-1:]
+            df_mov["horario_mov"] = datetime.datetime.now().strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
             df_mov.loc[
                 df_estoque["codigoProduto"] == produto, "estoque"
             ] += quantidade_mov
@@ -61,6 +69,7 @@ class movEstoque:
             print(
                 f"Entrada de {quantidade_mov} unidades do produto '{produto}' realizada com sucesso."
             )
+            self.salvar_estoque_atual(df_estoque)
         elif tipo_mov.lower() == "saida":
             while (
                 df_estoque.loc[
@@ -79,12 +88,19 @@ class movEstoque:
                 ].values[0]
                 >= quantidade_mov
             ):
-                df_estoque.loc[
+                df_mov = df_estoque.loc[df_estoque["codigoProduto"] == produto]
+                df_mov = df_mov.iloc[-1:]
+                df_mov["horario_mov"] = datetime.datetime.now().strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                )
+                df_mov.loc[
                     df_estoque["codigoProduto"] == produto, "estoque"
                 ] -= quantidade_mov
+                df_estoque = pd.concat([df_estoque, df_mov], ignore_index=True)
                 print(
                     f"Saída de {quantidade_mov} unidades do produto '{produto}' realizada com sucesso."
                 )
+                self.salvar_estoque_atual(df_estoque)
 
         else:
             print("Tipo de movimentação inválido. Use 'entrada' ou 'saida'.")
@@ -92,6 +108,18 @@ class movEstoque:
         print("\nEstoque atualizado:")
         print(df_estoque)
 
+    def salvar_estoque_atual(self, dataframe):
+        """Salva o dataframe atualizado de estoque em um arquivo JSON."""
+        df_estoque = dataframe
+        estoque_dict = {"estoque": df_estoque.to_dict(orient="records")}
+        # estoque_dict = pd.DataFrame(estoque_dict)
+        with open(self.estoque_atual_json, "w") as f:
+            json.dump(estoque_dict, f, indent=4)
 
-# print(movEstoque().df_estoque_atual())
-# print(movEstoque().item_selecionado(106))
+    def reset_estoque_atual(self):
+        """Reseta o estoque atual para o estoque inicial."""
+        df_estoque_inicial = self.df_estoque_inicial()
+        estoque_dict = {"estoque": df_estoque_inicial.to_dict(orient="records")}
+        with open(self.estoque_atual_json, "w") as f:
+            json.dump(estoque_dict, f, indent=4)
+        print("Estoque atual resetado para o estoque inicial com sucesso.")
--- desafio_dois/main.py
+++ desafio_dois/main.py
@@ -1,61 +1,76 @@
-import json
+from infrastructure.df_estoque_mov import movEstoque
 
-with open("desafio_dois/dados/estoque_inicial.json", "r") as file:
-    data = json.load(file)
-
-estoque_inicial = data["estoque"]
-
-
+moveestoque = movEstoque()
 mov_loop = 1
+contagem = 0
 while mov_loop == 1:
+    contagem += 1
+    loop_interno = 1
     inicio = input("Deseja iniciar uma movimentação? (s/n)")
     if inicio.lower() == "s":
         print("\n\nEstoque Inicial: \n")
-        for i in estoque_inicial:
-            print(
-                "Produto:",
-                i["codigoProduto"],
-                "-",
-                i["descricaoProduto"],
-                "-",
-                " Estoque: ",
-                i["estoque"],
+        if contagem == 1:
+            estoque_atual = moveestoque.df_estoque_inicial()
+        else:
+            estoque_atual = moveestoque.df_estoque_atual()
+        print(estoque_atual)
+        while loop_interno == 1:
+            produto_selecionado = input("\nDigite o código do produto: \n")
+            try:
+                produto_selecionado = int(produto_selecionado)
+            except ValueError:
+                print("Código inválido.")
+                while not isinstance(produto_selecionado, int):
+                    try:
+                        produto_selecionado = int(
+                            input("Digite o código do produto: \n")
+                        )
+                    except ValueError:
+                        print("Código não encontrado na tabela.")
+            loop_interno, produto_movido = moveestoque.item_selecionado(
+                produto_selecionado
             )
-        mov = input("\nDigite 'e' para entrada ou 's' para saída de produtos: ")
-        qtidade_mov = int(input("Digite a quantidade a ser movimentada: "))
-        while qtidade_mov <= 0:
-            print("Quantidade inválida. Deve ser maior que zero.")
-            qtidade_mov = int(input("Digite a quantidade a ser movimentada: "))
-        cod_produto = int(input("Digite o código do produto a ser movimentado: "))
-        while cod_produto not in [item["codigoProduto"] for item in estoque_inicial]:
-            print("Código de produto inválido.")
-            cod_produto = int(input("Digite o código do produto a ser movimentado: "))
-            continue
-        print(f"\nMovimentando produto...\n {cod_}")
-        for item in estoque_inicial:
-            if item["codigoProduto"] == cod_produto:
-                if mov.lower() == "e":
-                    item["estoque"] += qtidade_mov
-                    print(
-                        f"Entrada de {qtidade_mov} unidades do produto '{cod_produto}' realizada com sucesso."
-                    )
-                elif mov.lower() == "s":
-                    if item["estoque"] >= qtidade_mov:
-                        item["estoque"] -= qtidade_mov
-                        print(
-                            f"Saída de {qtidade_mov} unidades do produto '{cod_produto}' realizada com sucesso."
+            if loop_interno == 0:
+                break
+            mov = input("\nDigite 'e' para entrada ou 's' para saída de produtos: ")
+            while mov.lower() not in ["e", "s"]:
+                mov = input(
+                    "Tipo de movimentação inválido. Use 'e' para entrada ou 's' para saída."
+                )
+            qtidade_mov = input("Digite a quantidade a ser movimentada: \n")
+            try:
+                qtidade_mov = int(qtidade_mov)
+            except ValueError:
+                print("Quantidade inválida. Deve ser um número inteiro maior que zero.")
+                while not isinstance(qtidade_mov, int):
+                    try:
+                        qtidade_mov = int(
+                            input("Digite a quantidade a ser movimentada: \n")
                         )
-                    else:
+                    except ValueError:
                         print(
-                            f"Erro: Quantidade insuficiente em estoque para o produto '{cod_produto}'."
+                            "Quantidade inválida. Deve ser um número inteiro maior que zero."
                         )
-                else:
-                    print(
-                        "Tipo de movimentação inválido. Use 'e' para entrada ou 's' para saída."
-                    )
 
+            while qtidade_mov <= 0:
+                try:
+                    qtidade_mov = int(qtidade_mov)
+                finally:
+                    print("Quantidade inválida. Deve ser maior que zero.")
+                    qtidade_mov = input("Digite a quantidade a ser movimentada: ")
+
+            #
+            #
+            moveestoque.movimentar_estoque(
+                produto_movido["codigoProduto"].values[0],
+                "entrada" if mov.lower() == "e" else "saida",
+                qtidade_mov,
+            )
+            loop_interno = 0
+        mov_loop = 1
     elif inicio.lower() == "n":
         print("Encerrando o programa.")
+        moveestoque.reset_estoque_atual()
         mov_loop = 0
     else:
         print("Entrada inválida. Por favor, digite 's' ou 'n'.")
